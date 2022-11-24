cd17c07f150e6e8fb5d89ead8a195b0935fcc740
per-sever <a.petrin107@gmail.com>
2022-11-24 19:22:14 +0300

    the way of parsing objects was changed

--- app/src/main/java/com/example/numbercomposition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/example/numbercomposition/presentation/GameFinishedFragment.kt
@@ -42,6 +42,7 @@ class GameFinishedFragment : Fragment() {
             }
         }
         requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+        binding.btnTryAgain.setOnClickListener { retryGame() }
     }
 
     private fun retryGame() {
@@ -57,7 +58,9 @@ class GameFinishedFragment : Fragment() {
     }
 
     private fun parseArgs() {
-        gameResult = requireArguments().getSerializable(KEY_GAME_RESULT) as GameResult
+        requireArguments().getParcelable<GameResult>(KEY_GAME_RESULT)?.let {
+            gameResult = it
+        }
     }
 
     companion object {
@@ -66,7 +69,7 @@ class GameFinishedFragment : Fragment() {
         fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
                 arguments = Bundle().apply {
-                    putSerializable(KEY_GAME_RESULT, gameResult)
+                    putParcelable(KEY_GAME_RESULT, gameResult)
                 }
             }
         }
