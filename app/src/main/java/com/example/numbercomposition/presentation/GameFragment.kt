48f966483e6d0f3832aee5b842c2be18e852b60f
per-sever <a.petrin107@gmail.com>
2022-11-24 17:01:06 +0300

    navigation through FragmentManager was added

--- app/src/main/java/com/example/numbercomposition/presentation/GameFragment.kt
+++ app/src/main/java/com/example/numbercomposition/presentation/GameFragment.kt
@@ -5,13 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.numbercomposition.R
 import com.example.numbercomposition.databinding.FragmentGameBinding
+import com.example.numbercomposition.domain.entity.GameResult
+import com.example.numbercomposition.domain.entity.GameSettings
+import com.example.numbercomposition.domain.entity.Level
 
 class GameFragment : Fragment() {
+
+    private lateinit var level: Level
 
     private var _binding: FragmentGameBinding? = null
     private val binding: FragmentGameBinding
         get() = _binding ?: throw RuntimeException("FragmentGameBinding == null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -25,5 +36,51 @@ class GameFragment : Fragment() {
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvOption1.setOnClickListener {
+            launchGameFinishedFragment(
+                GameResult(
+                    false,
+                    0,
+                    0,
+                    GameSettings(
+                        0,
+                        0,
+                        0,
+                        0
+                    )
+                )
+            )
+        }
+    }
+
+    private fun parseArgs() {
+        level = requireArguments().getSerializable(KEY_LEVEL) as Level
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction().replace(
+            R.id.main_container,
+            GameFinishedFragment.newInstance(gameResult)
+        ).addToBackStack(null)
+            .commit()
+    }
+
+    companion object {
+
+        private const val KEY_LEVEL = "level"
+
+        const val NAME = "GameFragment"
+
+        fun newInstance(level: Level): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putSerializable(KEY_LEVEL, level)
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/numbercomposition/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/numbercomposition/presentation/WelcomeFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.numbercomposition.R
 import com.example.numbercomposition.databinding.FragmentWelcomeBinding
 
 class WelcomeFragment : Fragment() {
@@ -20,6 +21,20 @@ class WelcomeFragment : Fragment() {
     ): View {
         _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnUnderstood.setOnClickListener {
+            launchChooseLevelFragment()
+        }
+    }
+
+    private fun launchChooseLevelFragment() {
+        requireActivity().supportFragmentManager.beginTransaction().replace(
+            R.id.main_container,
+            ChooseLevelFragment.newInstance()
+        ).addToBackStack(null).commit()
     }
 
     override fun onDestroyView() {
